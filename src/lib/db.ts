6386efa6a30895fd1728f7f5deeef493f09067b4
Kirill Nagulin <over90006at9@gmail.com>
2025-09-20 00:07:18 +0300

    db and schema initialized

---
+++ src/lib/db.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from "@prisma/client";
+
+const PrismaSingleton = () =>{
+    return new PrismaClient({
+
+    })
+}
+
+declare global {
+    var prisma: undefined | ReturnType<typeof PrismaSingleton>;
+}
+
+const prisma = globalThis.prisma || PrismaSingleton();
+export default prisma;
+
